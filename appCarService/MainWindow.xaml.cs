d36318f7c11f1e0896ca932e89521ef7499efec2
Danil Vetrov <danil-vetrov47@mail.ru>
2022-04-03 12:51:04 +0400

    написал таймер на ввод, сделал функции на вывод месседж бокса,

--- appCarService/MainWindow.xaml.cs
+++ appCarService/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace appCarService
 {
@@ -23,16 +26,63 @@ namespace appCarService
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
+        DispatcherTimer timer = new DispatcherTimer();
+        int counter = 0;
         private void btnEnter_Click(object sender, RoutedEventArgs e)
         {
+            ModelBD.BaseModel bd = new ModelBD.BaseModel();
+            bd.Users.Load();
+            if (bd.Users.Local.Where(x => x.login == tbLogin.Text & x.password == tbPass.Password).FirstOrDefault() != null)
+            {
+                MessageBox.Show("вы вошли");
+            }
+            else
+            {
+                Exp("Пользователя с такими данными нет!");
+                counter++;
+            }
+            if (counter >= 3)
+            {
+                Inf("Вы ввели 3 раза неправильно пароль\nВвод пароля будет доступен через 10 секунд");
+                counter = 0;
+                timer = new DispatcherTimer();
+                timer.Tick += new EventHandler(timer_Tick);
+                timer.Interval = new TimeSpan(0, 0, 10);
+                btnEnter.Visibility = Visibility.Hidden;
+                timer.Start();
+            }
+        }
 
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            btnEnter.Visibility = Visibility.Visible;
         }
 
         private void btnQuit_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        public void Exp(string error)
+        {
+            MessageBox.Show(error, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        public void Inf(string inf)
+        {
+            MessageBox.Show(inf, "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void tbLogin_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex input = new Regex(@"[a-zA-Z]");
+            Match match = input.Match(e.Text);
+            if (!match.Success)
+            {
+                e.Handled = true;
+            }
         }
     }
 }
