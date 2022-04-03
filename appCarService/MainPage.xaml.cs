056e5d1d58c1e4cbd5a831516fdc667cc56aea38
Danil Vetrov <danil-vetrov47@mail.ru>
2022-04-03 13:33:58 +0400

    Поставил иконку на второе окно, создал новую страницу и подвезал через класс фрейм

--- appCarService/MainPage.xaml.cs
+++ appCarService/MainPage.xaml.cs
@@ -10,28 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace appCarService
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainPage : Page
     {
-        public MainWindow()
+        public MainPage()
         {
             InitializeComponent();
-            if (Manager.Admin == true)
-            {
-                tbRole.Text = "Вы вошли как менеджер";
-            }
-            else
-            {
-                tbRole.Text = "Вы вошли как клиент";
-            }
-            Manager.MainFrame = MainFrame;
-            Manager.MainFrame.Navigate(new MainPage());
         }
     }
 }
