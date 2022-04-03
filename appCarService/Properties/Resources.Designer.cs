d325001bc974f9d54f1943fa052b0894b217df7b
Danil Vetrov <danil-vetrov47@mail.ru>
2022-04-03 13:46:13 +0400

    Добавил картинку, начал описывать вывод товара через листвью

--- appCarService/Properties/Resources.Designer.cs
+++ appCarService/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace appCarService.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap noimage {
+            get {
+                object obj = ResourceManager.GetObject("noimage", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap service_logo {
             get {
                 object obj = ResourceManager.GetObject("service_logo", resourceCulture);
