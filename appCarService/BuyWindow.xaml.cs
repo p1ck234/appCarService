5a904f639b701b3aae4b4b2d2bc125a35bc63afc
Danil Vetrov <danil-vetrov47@mail.ru>
2022-04-04 14:01:18 +0400

    сделал вывод в бд продуксаейл

--- appCarService/BuyWindow.xaml.cs
+++ appCarService/BuyWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,25 @@ namespace appCarService
         {
             InitializeComponent();
             tbBuy.Text += "\"" + TovarPage.selectEntites.Title + "\"" + "?";
+        }
+
+        private void btnYes_Click(object sender, RoutedEventArgs e)
+        {
+            ModelBD.BaseModel bd = new ModelBD.BaseModel();
+            bd.ProductSales.Load();
+            ModelBD.ProductSale bd2 = new ModelBD.ProductSale();
+            
+            bd2.ID = bd.ProductSales.Local.Count + 1;
+            bd2.SaleDate = DateTime.Now;
+            bd2.ProductID = TovarPage.selectEntites.ID;
+            bd2.Quantity = 1;
+            bd.ProductSales.Add(bd2);
+            bd.SaveChanges();
+        }
+
+        private void btnNo_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
