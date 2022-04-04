bbec2a4282fc1a0338fede04a4c9074208eb9f17
Danil Vetrov <danil-vetrov47@mail.ru>
2022-04-04 14:11:00 +0400

    Сделал окно покупки

--- appCarService/BuyWindow.xaml.cs
+++ appCarService/BuyWindow.xaml.cs
@@ -24,6 +24,15 @@ namespace appCarService
         {
             InitializeComponent();
             tbBuy.Text += "\"" + TovarPage.selectEntites.Title + "\"" + "?";
+            List<int> a = new List<int>();
+            a.Add(1);
+            a.Add(2);
+            a.Add(3);
+            a.Add(4);
+            a.Add(5);
+            cmbCount.ItemsSource = a;
+            cmbCount.SelectedIndex = 0;
+
         }
 
         private void btnYes_Click(object sender, RoutedEventArgs e)
@@ -35,9 +44,16 @@ namespace appCarService
             bd2.ID = bd.ProductSales.Local.Count + 1;
             bd2.SaleDate = DateTime.Now;
             bd2.ProductID = TovarPage.selectEntites.ID;
-            bd2.Quantity = 1;
-            bd.ProductSales.Add(bd2);
-            bd.SaveChanges();
+            bd2.Quantity = (int)cmbCount.SelectedItem;
+            try
+            {
+                bd.ProductSales.Add(bd2);
+                bd.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnNo_Click(object sender, RoutedEventArgs e)
