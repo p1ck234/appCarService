9b923520615cdf5574046aaf0022aede7893728a
Danil Vetrov <danil-vetrov47@mail.ru>
2022-04-05 22:17:33 +0400

    исправил баг с картинками

--- appCarService/Properties/Resources.Designer.cs
+++ appCarService/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace appCarService.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap _1 {
+            get {
+                object obj = ResourceManager.GetObject("_1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap noimage {
             get {
                 object obj = ResourceManager.GetObject("noimage", resourceCulture);
