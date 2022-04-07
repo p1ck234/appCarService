39b5bcfa04b5a5b6409bc60badcc359d711885e6
Danil Vetrov <danil-vetrov47@mail.ru>
2022-04-07 19:26:20 +0400

    сделал описание для товара

--- appCarService/Properties/Resources.Designer.cs
+++ appCarService/Properties/Resources.Designer.cs
@@ -73,16 +73,6 @@ namespace appCarService.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap noimage {
-            get {
-                object obj = ResourceManager.GetObject("noimage", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap service_logo {
             get {
                 object obj = ResourceManager.GetObject("service_logo", resourceCulture);
